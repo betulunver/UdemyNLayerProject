a230c42148d6a6beb60a2506f26d64481bd39334
Betul UNVER <betul.unver@bilgeadam.com>
2021-06-21 23:02:21 +0300

    configurations added

--- UdemyNLayerProject.Data/AppDbContext.cs
+++ UdemyNLayerProject.Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using UdemyNLayerProject.Core.Entities;
+using UdemyNLayerProject.Data.Configurations;
 
 namespace UdemyNLayerProject.Data
 {
@@ -15,6 +16,10 @@ namespace UdemyNLayerProject.Data
             public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //Burayı temiz tutmak adına configuration'lar ayrı dosyada tutuldu 
+            //örn: modelBuilder.Entity<Product>().HasKey(x => x.Id);
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
