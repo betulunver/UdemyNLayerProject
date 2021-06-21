005a31ec3c56747196573d8d67509cc785c151e7
Betul UNVER <betul.unver@bilgeadam.com>
2021-06-21 23:50:51 +0300

    repository files added.

---
+++ UdemyNLayerProject.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Entities;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Data.Repositories
+{
+    class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private AppDbContext appDbContext { get => _context as AppDbContext; }
+        public ProductRepository(DbContext context) : base(context)
+        {
+        }
+
+        public async Task<Product> GetwithCategoryByProductId(int productId)
+        {
+            return await appDbContext.Products.Include(x => x.Category).SingleOrDefaultAsync(x=>x.Id==productId);
+        }
+    }
+}
---
+++ UdemyNLayerProject.Data/Repositories/Repository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Data.Repositories
+{
+   public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public readonly DbContext _context;
+        public readonly DbSet<TEntity> _dbSet;
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public IEnumerable<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int Id)
+        {
+            return await _dbSet.FindAsync(Id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.SingleOrDefaultAsync(predicate);
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            //tüm sütunları update eder. Ama perfomans kaybı olabilir
+            _context.Entry(entity).State = EntityState.Modified;
+            //eğer ki böyle olmasını istemiyorsak ilgili alanları tek tek atamamız gerekir.
+            //Ama çok fazla sütunlarda bu işlem yapılamayazağından performans kaybını gözadı ederiz.
+            //örn : entity.name=product.name;
+            return entity;
+           
+        }
+    }
+}
