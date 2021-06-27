5120cdc3077f2ecdfed0ab79dfa3f64765263f84
Betul UNVER <betul.unver@bilgeadam.com>
2021-06-27 17:17:40 +0300

    Add person entity. Apply migration.

--- UdemyNLayerProject.Data/AppDbContext.cs
+++ UdemyNLayerProject.Data/AppDbContext.cs
@@ -15,15 +15,18 @@ namespace UdemyNLayerProject.Data
         }
             public DbSet<Category> Categories { get; set; }
             public DbSet<Product> Products { get; set; }
+            public DbSet<Person> Persons { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Burayı temiz tutmak adına configuration'lar ayrı dosyada tutuldu 
             //örn: modelBuilder.Entity<Product>().HasKey(x => x.Id);
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new PersonConfiguration());
 
             modelBuilder.ApplyConfiguration(new ProductSeed(new int[] { 1,2}));
             modelBuilder.ApplyConfiguration(new CategorySeed(new int[] { 1, 2 }));
+
         }
     }
 }
