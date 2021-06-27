3101783caff40f7ad8178b3ee0e906d26bead770
Betul UNVER <betul.unver@bilgeadam.com>
2021-06-27 14:17:52 +0300

    AutoMapper library added.

---
+++ UdemyNLayerProject.API/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNLayerProject.API.DTOs;
+using UdemyNLayerProject.Core.Services;
+
+namespace UdemyNLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        //api/category şeklinde get isteği atarsak buraya düşecektir. best practice açısından entity isimleri olmalı 
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+    }
+}
